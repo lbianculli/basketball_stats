17025a24954a739bd72f7b08bdc25f0cfd7214b4
lbianculli <35279143+lbianculli@users.noreply.github.com>
2018-12-21 21:35:03 -0500

    Update base.py

--- base.py
+++ base.py
@@ -1,15 +1,23 @@
-import pandas as pd
 import bs4 as bs
+import collections
+import pandas as pd
 from urllib.request import urlopen
+import re
+import time
 import functools
+import numpy as np
+pd.options.mode.chained_assignment = None
 
 
-class BrefStats():    
+class SingleSeasonStats(): 
+    '''
+    Class that creates pd.DataFrames of NBA players' stats -- basic, advanced, or both -- for a given year.
+    '''
     def __init__(self, year):
         self.year = year
-        self.basic_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_per_game.html'
-        self.adv_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_advanced.html'
-        self.per_poss_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_per_poss.html'
+        self.all_nba_dict = {}
+        self.CACHE_FLAG = False
+        
 
     def _gen_dataframe(self, url): 
         '''
@@ -36,86 +44,89 @@ class BrefStats():
 
         return df
 
-    
     @functools.lru_cache(maxsize=32)
-    def _gen_basic(self): 
+    def basic_stats(self): 
         '''
         Retrieves all players and their basic stats DF for given season
         '''
-        basic_df = self._gen_dataframe(self.basic_url)
+        basic_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_per_game.html'
+        basic_df = self._gen_dataframe(basic_url)
+        
         return basic_df
 
-
-    def _gen_netrtg(self):
-        '''
-        Retrieves ORTG, DRTG from per100 stats so that Net column can be created (in wrapper())
-        '''
-        all_per100 = self._gen_dataframe(self.per_poss_url)
-
-        ortg = all_per100['ORtg'].replace('', np.nan)
-        drtg = all_per100['DRtg'].replace('', np.nan)
-
-        rating_df = pd.DataFrame()
-        rating_df['ORtg'] = pd.Series(ortg)
-        rating_df['DRtg'] = pd.Series(drtg)
-
-        return rating_df
-
     @functools.lru_cache(maxsize=32)
-    def _gen_adv(self):
+    def adv_stats(self):
         """
-        Generate advanced stats DF for a player, inlcuding ORTG, DRTG
+        Generate advanced stats DF for a player, inlcuding ORTG, DRTG, Net 
         """
         
-        adv = self._gen_dataframe(self.adv_url)
-        net = self._gen_netrtg()
-        adv_df = pd.concat((adv, net), axis=1)
-        adv_df['MP'] = adv_df['MP'].astype(int)
+        adv_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_advanced.html'
+        per_poss_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_per_poss.html'
+        
+        adv_df = self._gen_dataframe(adv_url)
+        all_per100 = self._gen_dataframe(per_poss_url)
 
-        return adv_df
+        ortg = all_per100['ORtg'].replace('', np.nan)
+        drtg = all_per100['DRtg'].replace('', np.nan) 
+        adv_df['ORtg'] = ortg
+        adv_df['DRtg'] = drtg
+        adv_df.drop('x', axis=1, inplace=True)
+
+        adv_df.loc[:, 'ORtg'].replace('', np.nan) 
+        adv_df.dropna(inplace = True)
+        adv_df['Net Rtg'] = adv_df['ORtg'].astype(int) - adv_df['DRtg'].astype(int)
     
+        return adv_df
 
     @functools.lru_cache(maxsize=32)
-    def _combine(self): 
+    def combine(self): 
         '''
         Tries to combine adv and basic DFs. Otherwise returns just basic
         '''
-        basic_df = self._gen_basic()
+        basic_df = self.basic_stats()
         basic_df = basic_df.drop(basic_df.columns.to_series()['Pos':'DRB'], axis=1)
 
         try:
 
-            adv_df = self._gen_adv()
-            adv_df.drop('x', axis=1, inplace=True) 
-
-            com_df = pd.concat((adv_df, basic_df), axis=1)
-
-            com_df.loc[:, 'ORtg'].replace('', np.nan) 
-            com_df.dropna(inplace = True)
-            com_df['Net Rtg'] = com_df['ORtg'].astype(int) - com_df['DRtg'].astype(int)
-            com_df['MP'] = com_df['MP'].astype(int)
-   
+            adv_df = self.adv_stats()
+            com_df = pd.concat((basic_df, adv_df), axis=1, sort=True)
+            self._add_label(com_df)
+            com_df = com_df.drop(com_df.columns.to_series()['Pos':'MP'], axis=1)
+            
             return com_df
 
         except Exception as e:
             print(e)
+            self._add_label(basic_df)
             return basic_df
+        
+        
+    def gen_all_nba(self):
+        '''
+        Generates, returns, and caches the all-nba player list of input year
+        '''
+        url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year)+'.html'
+        allnba_players = []
+        soup = bs.BeautifulSoup(urlopen(url), 'lxml')
+        spans = [span.get_text() for span in soup.find_all('span')]
+        all_nba = soup.find('div', id='all_honors')
+        players = re.findall(r"'>(\w*[-\s]\w*['\s-]*\w*)", str(all_nba))
+        
+        self.all_nba_dict[self.year] = players
 
-    
-    def _add_label(self):
+    def _add_label(self, df):
         '''
         returns new df with binary col indicating whether player made all-nba that year by comparing to cache
         of all-nba players
         '''
-        combined_df = _combine(year)
-        soup = bs.BeautifulSoup(urlopen(self.basic_url), 'lxml') # I think this is the page with basic stats
-        spans = [span.get_text() for span in soup.find_all('span')]
+        if self.CACHE_FLAG is False:
+            self.gen_all_nba()
+
         df['all_nba'] = 0
-
         for player in df.index:
-            if player in all_nba_dict[year]:
+            if player in self.all_nba_dict[self.year]:
                 df['all_nba'].loc[player] = 1
-
-        df = df.drop(df.columns.to_series()['Pos':'MP'], axis=1)
-
+        
+        self.CACHE_FLAG = True
         return df
+
