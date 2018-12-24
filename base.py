efe4607d99793afa2ad4df99f4eccc49f227efb5
lbianculli <35279143+lbianculli@users.noreply.github.com>
2018-12-23 20:56:45 -0500

    Update base.py

--- base.py
+++ base.py
@@ -8,37 +8,13 @@ import functools
 import numpy as np
 pd.options.mode.chained_assignment = None
 
+from utils import _gen_dataframe
+
 class SingleSeasonStats(): 
     def __init__(self, year):
         self.year = year
         self.all_nba_dict = {}
         self.CACHE_FLAG = False
-        
-
-    def _gen_dataframe(self, url): 
-        '''
-        Boilerplate DF generator
-        '''
-        cols = []
-        soup = bs.BeautifulSoup(urlopen(url), 'lxml')
-        table = soup.find('div', class_='table_outer_container')
-
-        for th in table.thead.find_all('th'):
-            if th.get_text() == '\xa0':
-                cols.append('x')
-            else:
-                cols.append(th.get_text())
-
-        n_cols = len(table.thead.find_all('th')) - 1
-        data = [td.get_text() for tr in table.tbody.find_all('tr', class_='full_table') for td in tr.find_all('td')]       
-        data = [data[i:i+n_cols] for i in range(0, len(data), n_cols)]
-
-        all_players = [sublist.pop(0).replace('*', '') for sublist in data]
-
-        cols = cols[2:]
-        df = pd.DataFrame(index=all_players, data=data, columns=cols)
-
-        return df
 
     @functools.lru_cache(maxsize=32)
     def basic_stats(self): 
@@ -46,7 +22,7 @@ class SingleSeasonStats():
         Retrieves all players and their basic stats DF for given season
         '''
         basic_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_per_game.html'
-        basic_df = self._gen_dataframe(basic_url)
+        basic_df = _gen_dataframe(basic_url)
         
         return basic_df
 
@@ -59,8 +35,8 @@ class SingleSeasonStats():
         adv_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_advanced.html'
         per_poss_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(self.year) + '_per_poss.html'
         
-        adv_df = self._gen_dataframe(adv_url)
-        all_per100 = self._gen_dataframe(per_poss_url)
+        adv_df = _gen_dataframe(adv_url)
+        all_per100 = _gen_dataframe(per_poss_url)
 
         ortg = all_per100['ORtg'].replace('', np.nan)
         drtg = all_per100['DRtg'].replace('', np.nan) 
@@ -153,11 +129,11 @@ class MultiSeasonStats(SingleSeasonStats):
         adv_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(start_year) + '_advanced.html'
         per_poss_url = 'https://www.basketball-reference.com/leagues/NBA_' + str(start_year) + '_per_poss.html'
 
-        basic_df = self._gen_dataframe(basic_url)
+        basic_df = _gen_dataframe(basic_url)
         basic_df = basic_df.drop(basic_df.columns.to_series()['Pos':'DRB'], axis=1)
 
-        adv_df = self._gen_dataframe(adv_url)
-        per_poss_df = self._gen_dataframe(per_poss_url)
+        adv_df = _gen_dataframe(adv_url)
+        per_poss_df = _gen_dataframe(per_poss_url)
     
         ortg = per_poss_df['ORtg'].replace('', np.nan)
         drtg = per_poss_df['DRtg'].replace('', np.nan) 
